0005c8bd9571b32a6f6dc75891ec4cdbdd2d7fff
NickFrol1 <nickfrol85@gmail.com>
2023-02-07 21:11:42 +0300

    2

--- tests/Myclass.java
+++ tests/Myclass.java
@@ -1,24 +1,24 @@
-//public class Myclass {
-////    public static void main(String[] args) {
-////        System.out.println("Hello from code");
-////    }
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
-
 public class Myclass {
-
-    @Test
-    public void whenThis_thenThat() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void whenSomething_thenSomething() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void whenSomethingElse_thenSomethingElse() {
-        assertTrue(true);
-    }
-}
+//    public static void main(String[] args) {
+//        System.out.println("Hello from code");
+//    }
+//import org.junit.Test;
+//import static org.junit.Assert.assertTrue;
+//
+//public class Myclass {
+//
+//    @Test
+//    public void whenThis_thenThat() {
+//        assertTrue(true);
+//    }
+//
+//    @Test
+//    public void whenSomething_thenSomething() {
+//        assertTrue(true);
+//    }
+//
+//    @Test
+//    public void whenSomethingElse_thenSomethingElse() {
+//        assertTrue(true);
+//    }
+//}
