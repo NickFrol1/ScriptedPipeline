c99d19d7445d4ad63b665880a912184eefa35a5f
NickFrol1 <nickfrol85@gmail.com>
2023-02-07 19:45:31 +0300

    2

--- tests/Myclass.java
+++ tests/Myclass.java
@@ -1,5 +1,21 @@
+//public class Myclass {
+////    public static void main(String[] args) {
+////        System.out.println("Hello from code");
+////    }
 public class Myclass {
-    public static void main(String[] args) {
-        System.out.println("Hello from code");
+
+    @Test
+    public void whenThis_thenThat() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void whenSomething_thenSomething() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void whenSomethingElse_thenSomethingElse() {
+        assertTrue(true);
     }
 }
